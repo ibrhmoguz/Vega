2192b94c9c8bf4ed42ff69bca5b48e8caf748c92
ibrhmoguz <ibrahimoguz.ie@gmail.com>
2018-04-10 15:06:30 +0300

    Initial data edited.

--- Persistence/VegaDbInitializer.cs
+++ Persistence/VegaDbInitializer.cs
@@ -16,25 +16,25 @@ namespace Vega.Persistence
             }
 
             var modelList1 = new List<Model>{
-                new Model{Name="Model1"},
-                new Model{Name="Model2"},
-                new Model{Name="Model3"},
-                new Model{Name="Model4"},
+                new Model{Name="E350"},
+                new Model{Name="E220"},
+                new Model{Name="SLA 120"},
+                new Model{Name="C180"},
             };
 
 
             var modelList2 = new List<Model>{
-                new Model{Name="Model5"},
-                new Model{Name="Model6"},
-                new Model{Name="Model7"}
+                new Model{Name="C5"},
+                new Model{Name="C4"},
+                new Model{Name="C3"}
             };
 
             modelList1.ForEach(m => dbContext.Models.Add(m));
             modelList2.ForEach(m => dbContext.Models.Add(m));
 
             var makeList = new List<Make>{
-                new Make{Name="Make1", Models= modelList1},
-                new Make{Name="Make2", Models= modelList2}
+                new Make{Name="Mercedes-Benz", Models= modelList1},
+                new Make{Name="Citroen", Models= modelList2}
             };
 
             makeList.ForEach(m => dbContext.Makes.Add(m));
