536299b597dae5d8d371d4369eda627b9d411fd8
ibrhmoguz <ibrahimoguz.ie@gmail.com>
2018-05-25 13:48:51 +0100

    Many-to-many relation Vehicle and Feature added.

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,14 +1,29 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Vega.Models
 {
+    [Table("Vehicle")]
     public class Vehicle
     {
         public int Id { get; set; }
         public bool IsRegistered { get; set; }
-        public string ContactName { get; set; }
-        public string Contact { get; set; }
-        public Make Make { get; set; }
-        public int MakeId { get; set; }
         public Model Model { get; set; }
         public int ModelId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactMail { get; set; }
+
+        public ICollection<VehicleFeature> Features { get; set; }
     }
 }
